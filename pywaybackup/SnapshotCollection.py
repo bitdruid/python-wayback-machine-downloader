946d27268fc2e91d3f6b22ae39741c9acf29b082
bitdruid <bitdruid@outlook.com>
2024-03-14 15:00:00 +0100

    make singleton

--- pywaybackup/SnapshotCollection.py
+++ pywaybackup/SnapshotCollection.py
@@ -3,24 +3,25 @@ import os
 
 class SnapshotCollection:
 
-    CDX_JSON = []
-    CDX_LIST = []
+    _instance = None
 
-    SNAPSHOT_COLLECTION = []
-
-    MODE_CURRENT = 0
+    def __new__(cls):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+        return cls._instance
 
     def __init__(self):
-        pass
+        self.CDX_JSON = []
+        self.CDX_LIST = []
+        self.SNAPSHOT_COLLECTION = []
+        self.MODE_CURRENT = 0
 
     def create_full(self, cdxResult):
         self.CDX_JSON = cdxResult.json()[1:]
-        self.CDX_LIST = [{"timestamp": snapshot[0], "url": snapshot[1]} for i, snapshot in enumerate(self.CDX_JSON)]
-        self.CDX_LIST = sorted(self.CDX_LIST, key=lambda k: k['timestamp'], reverse=True)
+        self.CDX_LIST = sorted([{"timestamp": snapshot[0], "url": snapshot[1]} for i, snapshot in enumerate(self.CDX_JSON)], key=lambda k: k['timestamp'], reverse=True)
 
     def create_current(self):
         self.MODE_CURRENT = 1
-        self.CDX_LIST = sorted(self.CDX_LIST, key=lambda k: k['timestamp'], reverse=True)
         cdxResult_list_filtered = []
         url_set = set()
         for snapshot in self.CDX_LIST:
@@ -32,39 +33,41 @@ class SnapshotCollection:
     def create_entry(self, cdx_entry: dict, output: str) -> dict:
         timestamp, url = cdx_entry["timestamp"], cdx_entry["url"]
         domain, subdir, filename = self.split_url(url)
-        if self.MODE_CURRENT: download_dir = os.path.join(output, domain, subdir)
-        else: download_dir = os.path.join(output, domain, timestamp, subdir)
+        if self.MODE_CURRENT:
+            download_dir = os.path.join(output, domain, subdir)
+        else:
+            download_dir = os.path.join(output, domain, timestamp, subdir)
         download_file = os.path.join(download_dir, filename)
         cdx_entry = {
-                "id": len(self.SNAPSHOT_COLLECTION),
-                "url": self.create_archive_url(timestamp, url),
-                "file": download_file,
-                "timestamp": timestamp,
-                "origin_url": url,
-                "success": False,
-                "retry": 0
-            }
+            "id": len(self.SNAPSHOT_COLLECTION),
+            "url": self.create_archive_url(timestamp, url),
+            "file": download_file,
+            "timestamp": timestamp,
+            "origin_url": url,
+            "success": False,
+            "retry": 0
+        }
         return cdx_entry
 
-    @classmethod    
+    @classmethod
     def create_archive_url(cls, timestamp: str, url: str) -> str:
-        url_type = cls.__get_url_filetype(url)
+        url_type = cls.get_url_filetype(url)
         return f"http://web.archive.org/web/{timestamp}{url_type}/{url}"
 
     def count_list(self):
         return len(self.CDX_LIST)
-    
+
     def snapshot_collection_write(self, query_entry: dict):
         if query_entry["id"] not in self.SNAPSHOT_COLLECTION:
             self.SNAPSHOT_COLLECTION.append(query_entry)
-    
+
     def snapshot_collection_update(self, id: int, key: str, value: str):
         index = next((index for (index, d) in enumerate(self.SNAPSHOT_COLLECTION) if d["id"] == id), None)
         if index is not None:
             self.SNAPSHOT_COLLECTION[index][key] = value
 
-    @classmethod
-    def get_url_filetype(cls, url):
+    @staticmethod
+    def get_url_filetype(url):
         file_extension = os.path.splitext(url)[1][1:]
         urltype_mapping = {
             "jpg": "im_",
@@ -79,7 +82,7 @@ class SnapshotCollection:
         urltype = urltype_mapping.get(file_extension, "id_")
         return urltype
 
-    @staticmethod    
+    @staticmethod
     def split_url(url):
         parsed_url = urlparse(url)
         domain = parsed_url.netloc
