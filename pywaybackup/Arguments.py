712729e61d160bb2b18376f9e817853a44c25929
username <username>
2024-11-20 00:42:36 +0000

    added first mode, quote fixes

--- pywaybackup/Arguments.py
+++ pywaybackup/Arguments.py
@@ -8,19 +8,20 @@ from pywaybackup.helper import url_split, sanitize_filename
 from pywaybackup.__version__ import __version__
 
 class Arguments:
-
+    
     def __init__(self):
-
+        
         parser = argparse.ArgumentParser(description='Download from wayback machine (archive.org)')
         parser.add_argument('-a', '--about', action='version', version='%(prog)s ' + __version__ + ' by @bitdruid -> https://github.com/bitdruid')
-
+        
         required = parser.add_argument_group('required (one exclusive)')
         required.add_argument('-u', '--url', type=str, metavar="", help='url (with subdir/subdomain) to download')
         exclusive_required = required.add_mutually_exclusive_group(required=True)
         exclusive_required.add_argument('-c', '--current', action='store_true', help='download the latest version of each file snapshot')
         exclusive_required.add_argument('-f', '--full', action='store_true', help='download snapshots of all timestamps')
+        exclusive_required.add_argument('-i', '--first', action='store_true', help='download the first version of each file snapshot')
         exclusive_required.add_argument('-s', '--save', action='store_true', help='save a page to the wayback machine')
-
+        
         optional = parser.add_argument_group('optional query parameters')
         optional.add_argument('-e', '--explicit', action='store_true', help='search only for the explicit given url')
         optional.add_argument('-r', '--range', type=int, metavar="", help='range in years to search')
@@ -28,7 +29,7 @@ class Arguments:
         optional.add_argument('--end', type=int, metavar="", help='end timestamp format: YYYYMMDDhhmmss')
         optional.add_argument('--filetype', type=str, metavar="", help='filetypes to download comma separated (e.g. "html,css")')
         optional.add_argument('--limit', type=int, nargs='?', const=True, metavar='int', help='limit the number of snapshots to download')
-
+        
         behavior = parser.add_argument_group('manipulate behavior')
         behavior.add_argument('-o', '--output', type=str, metavar="", help='output folder - defaults to current directory')
         behavior.add_argument('--log', action='store_true', help='save a log file into the output folder')
@@ -39,57 +40,59 @@ class Arguments:
         behavior.add_argument('--workers', type=int, default=1, metavar="", help='number of workers (simultaneous downloads)')
         # behavior.add_argument('--convert-links', action='store_true', help='Convert all links in the files to local paths. Requires -c/--current')
         behavior.add_argument('--delay', type=int, default=0, metavar="", help='delay between each download in seconds')
-
+        
         special = parser.add_argument_group('special')
         special.add_argument('--reset', action='store_true', help='reset the job and ignore existing cdx/db/csv files')
         special.add_argument('--keep', action='store_true', help='keep all files after the job finished')
-
+        
         args = parser.parse_args(args=None if sys.argv[1:] else ['--help']) # if no arguments are given, print help
-
+        
         required_args = {action.dest: getattr(args, action.dest) for action in exclusive_required._group_actions}
         optional_args = {action.dest: getattr(args, action.dest) for action in optional._group_actions}
         args.query_identifier = str(args.url) + str(required_args) + str(optional_args)
-
+        
         # if args.convert_links and not args.current:
         #     parser.error("--convert-links can only be used with the -c/--current option")
-
+        
         self.args = args
-
+    
     def get_args(self):
         return self.args
     
 class Configuration:
-
+    
     @classmethod
     def init(cls):
-
+        
         cls.args = Arguments().get_args()
         for key, value in vars(cls.args).items():
             setattr(Configuration, key, value)
-
+        
         # args now attributes of Configuration // Configuration.output, ...
         cls.command = ' '.join(sys.argv[1:])
         cls.domain, cls.subdir, cls.filename = url_split(cls.url)
-
+        
         if cls.output is None:
             cls.output = os.path.join(os.getcwd(), "waybackup_snapshots")
         os.makedirs(cls.output, exist_ok=True)
-
+        
         if cls.log is True:
             cls.log = os.path.join(cls.output, f"waybackup_{sanitize_filename(cls.url)}.log")
-
+        
         if cls.full:
             cls.mode = "full"
         if cls.current:
             cls.mode = "current"
-
+        if cls.first:
+            cls.mode = "first"
+        
         if cls.filetype:
             cls.filetype = [ft.lower().strip() for ft in cls.filetype.split(",")]
-
+        
         cls.cdxfile = os.path.join(cls.output, f"waybackup_{sanitize_filename(cls.url)}.cdx")
         cls.dbfile = os.path.join(cls.output, f"waybackup_{sanitize_filename(cls.url)}.db")
         cls.csvfile = os.path.join(cls.output, f"waybackup_{sanitize_filename(cls.url)}.csv")
-
+        
         if cls.reset:
             os.remove(cls.cdxfile) if os.path.isfile(cls.cdxfile) else None
             os.remove(cls.dbfile) if os.path.isfile(cls.dbfile) else None
--- pywaybackup/SnapshotCollection.py
+++ pywaybackup/SnapshotCollection.py
@@ -6,32 +6,37 @@ import json
 import csv
 import os
 
+
 class SnapshotCollection:
     """
     Represents the interaction with the snapshot-collection contained in the snapshot database.
     """
-
-    MODE_CURRENT = 0        # given by argument --current
-
-    CDX_TOTAL = 0           # absolute amount of snapshots in cdx file
-    SNAPSHOT_TOTAL = 0      # absolute amount of snapshots in db
-
-    SNAPSHOT_UNHANDLED = 0  # all unhandled snapshots in the db (without response)
-    SNAPSHOT_HANDLED = 0    # snapshots with a response
-
-    SNAPSHOT_FAULTY = 0     # snapshots which could not be loaded from cdx file into db
-    FILTER_DUPLICATES = 0   # with identical url_archive
-    FILTER_CURRENT = 0      # all snapshots without the latest timestamp of each
-    FILTER_SKIP = 0         # content of the csv file
-
+    
+    MODE_CURRENT = 0  # given by argument --current
+    
+    CDX_TOTAL = 0  # absolute amount of snapshots in cdx file
+    SNAPSHOT_TOTAL = 0  # absolute amount of snapshots in db
+    
+    # all unhandled snapshots in the db (without response)
+    SNAPSHOT_UNHANDLED = 0
+    SNAPSHOT_HANDLED = 0  # snapshots with a response
+    
+    SNAPSHOT_FAULTY = 0  # snapshots which could not be loaded from cdx file into db
+    FILTER_DUPLICATES = 0  # with identical url_archive
+    FILTER_CURRENT = 0  # all snapshots with the latest timestamp of each
+    FILTER_FIRST = 0  # all snapshots with the first timestamp of each
+    FILTER_SKIP = 0  # content of the csv file
+    
     @classmethod
     def init(cls, mode):
         """
         Initialize the snapshot collection by inserting the content of the cdx file into the snapshot table.
-        """        
-        if mode == "current": 
+        """
+        if mode == "current":
             cls.MODE_CURRENT = 1
-
+        if mode == "first":
+            cls.MODE_CURRENT = 2
+        
         cls.db = Database()
 
     @classmethod
@@ -42,11 +47,7 @@ class SnapshotCollection:
         cls.db.write_progress(cls.SNAPSHOT_HANDLED, cls.SNAPSHOT_TOTAL)
         cls.db.conn.commit()
         cls.db.conn.close()
-
-
-
-
-
+    
     @classmethod
     def process_cdx(cls, cdxfile, csvfile):
         """
@@ -58,29 +59,29 @@ class SnapshotCollection:
         if not cls.db.get_insert_complete():
             cls.insert_cdx(cdxfile)
             cls.db.set_insert_complete()
-        else: 
+        else:
             vb.write(message="\nAlready inserted CDX data into database")
         if not cls.db.get_index_complete():
             vb.write(message="\nIndexing snapshots...")
-            cls.index_snapshots() # create indexes for the snapshot table
+            cls.index_snapshots()  # create indexes for the snapshot table
             cls.db.set_index_complete()
-        else: 
+        else:
             vb.write(message="\nAlready indexed snapshots")
         if not cls.db.get_filter_complete():
             vb.write(message="\nFiltering current snapshots...")
-            cls.filter_current() # filter: remove duplicates (timestamp, url), keep latest (timestamp) snapshot of each file
+            # filter: remove duplicates (timestamp, url), keep latest (timestamp) snapshot of each file
+            cls.filter_current()
             cls.db.set_filter_complete()
-        else: 
+        else:
             vb.write(message="\nAlready filtered current snapshots")
-        cls.skip_set(csvfile) # set response to NULL or read csv file and write values into db
-        cls.SNAPSHOT_UNHANDLED = cls.count_totals(unhandled=True)   # count all unhandled in db
-        cls.SNAPSHOT_HANDLED = cls.count_totals(handled=True)       # count all handled in db
-        cls.SNAPSHOT_TOTAL = cls.count_totals(total=True)           # count all in db
-
-
-
-
-
+        # set response to NULL or read csv file and write values into db
+        cls.skip_set(csvfile)
+        cls.SNAPSHOT_UNHANDLED = cls.count_totals(
+            unhandled=True
+        )  # count all unhandled in db
+        cls.SNAPSHOT_HANDLED = cls.count_totals(handled=True)  # count all handled in db
+        cls.SNAPSHOT_TOTAL = cls.count_totals(total=True)  # count all in db
+    
     @classmethod
     def insert_cdx(cls, cdxfile):
         vb.write(message="\nInserting CDX data into database...")
@@ -91,14 +92,22 @@ class SnapshotCollection:
             faulty_lines = 0
             query = """INSERT OR IGNORE INTO snapshot_tbl (timestamp, url_archive, url_origin) VALUES (?, ?, ?)"""
             first_line = True
-            pbar = tqdm(unit=" lines", total=cls.CDX_TOTAL, desc="insert cdx".ljust(15), ascii="░▒█", bar_format='{l_bar}{bar:50}{r_bar}{bar:-10b}') # remove output: file=sys.stdout
+            pbar = tqdm(
+                unit=" lines",
+                total=cls.CDX_TOTAL,
+                desc="insert cdx".ljust(15),
+                ascii="░▒█",
+                bar_format="{l_bar}{bar:50}{r_bar}{bar:-10b}",
+            )  # remove output: file=sys.stdout
             for line in f:
                 if first_line:
                     first_line = False
                     continue
                 line = line.strip()
-                if line.endswith("]]"): line = line.rsplit("]", 1)[0]
-                if line.endswith(","): line = line.rsplit(",", 1)[0]
+                if line.endswith("]]"):
+                    line = line.rsplit("]", 1)[0]
+                if line.endswith(","):
+                    line = line.rsplit(",", 1)[0]
                 try:
                     line = json.loads(line)
                     line = {
@@ -106,10 +115,10 @@ class SnapshotCollection:
                         "digest": line[1],
                         "mimetype": line[2],
                         "status": line[3],
-                        "url": line[4]
+                        "url": line[4],
                     }
                     url_archive = f"https://web.archive.org/web/{line['timestamp']}id_/{line['url']}"
-                    line_batch.append((line['timestamp'], url_archive, line['url']))
+                    line_batch.append((line["timestamp"], url_archive, line["url"]))
                     if len(line_batch) >= line_batchsize:
                         total_inserted += len(line_batch)
                         cls.db.cursor.executemany(query, line_batch)
@@ -123,25 +132,27 @@ class SnapshotCollection:
                 total_inserted += len(line_batch)
                 cls.db.cursor.executemany(query, line_batch)
                 pbar.update(len(line_batch))
-
+            
             pbar.close()
         cls.db.conn.commit()
-
+        
         cls.SNAPSHOT_FAULTY = faulty_lines
-        cls.FILTER_DUPLICATES = (cls.CDX_TOTAL - cls.count_totals(unhandled=True) + cls.SNAPSHOT_FAULTY)
-
-
-
-
-
+        cls.FILTER_DUPLICATES = (
+            cls.CDX_TOTAL - cls.count_totals(unhandled=True) + cls.SNAPSHOT_FAULTY
+        )
+    
     @classmethod
     def csv_create(cls, csvfile):
         """
         Write a CSV file with the list of snapshots.
         """
         row_batchsize = 2500
-        cls.db.cursor.execute("UPDATE snapshot_tbl SET response = NULL WHERE response = 'LOCK'") # reset locked to unprocessed
-        cls.db.cursor.execute("SELECT * FROM snapshot_tbl WHERE response IS NOT NULL") # only write processed snapshots
+        # reset locked to unprocessed
+        cls.db.cursor.execute(
+            "UPDATE snapshot_tbl SET response = NULL WHERE response = 'LOCK'"
+        )
+        # only write processed snapshots
+        cls.db.cursor.execute("SELECT * FROM snapshot_tbl WHERE response IS NOT NULL")
         headers = [description[0] for description in cls.db.cursor.description]
         with open(csvfile, "w") as f:
             writer = csv.writer(f)
@@ -152,21 +163,21 @@ class SnapshotCollection:
                     break
                 writer.writerows(rows)
         cls.db.conn.commit()
-
-
-
-
-
+    
     @classmethod
     def index_snapshots(cls):
         """
         Create indexes for the snapshot table.
         """
         # index for filtering current snapshots
-        cls.db.cursor.execute("CREATE INDEX IF NOT EXISTS idx_snapshot_tbl_url_origin_timestamp ON snapshot_tbl(url_origin, timestamp DESC);")
+        cls.db.cursor.execute(
+            "CREATE INDEX IF NOT EXISTS idx_snapshot_tbl_url_origin_timestamp ON snapshot_tbl(url_origin, timestamp DESC);"
+        )
         # index for skippable snapshots
-        cls.db.cursor.execute("CREATE INDEX IF NOT EXISTS idx_snapshot_tbl_timestamp_url_origin_response ON snapshot_tbl(timestamp, url_origin);")
-
+        cls.db.cursor.execute(
+            "CREATE INDEX IF NOT EXISTS idx_snapshot_tbl_timestamp_url_origin_response ON snapshot_tbl(timestamp, url_origin);"
+        )
+    
     @classmethod
     def filter_current(cls):
         """
@@ -176,9 +187,9 @@ class SnapshotCollection:
         """
         # filter the snapshot table and keep only the latest (timestamp) snapshot of each file
         # rows get a row number based on the timestamp per url_origin and are deleted if the row number is greater than 1
-        if cls.MODE_CURRENT:
+        if cls.MODE_CURRENT == 1:
             cls.db.cursor.execute(
-            """
+                """
             DELETE FROM snapshot_tbl
             WHERE rowid IN (
                 SELECT rowid FROM (
@@ -191,13 +202,24 @@ class SnapshotCollection:
             """
             )
             cls.FILTER_CURRENT = cls.db.cursor.rowcount
-
+        if cls.MODE_CURRENT == 2:
+            cls.db.cursor.execute(
+                """
+            DELETE FROM snapshot_tbl
+            WHERE rowid IN (
+                SELECT rowid FROM (
+                    SELECT rowid,
+                        ROW_NUMBER() OVER (PARTITION BY url_origin ORDER BY timestamp ASC) AS rank
+                    FROM snapshot_tbl
+                ) tmp
+                WHERE rank > 1
+            );
+            """
+            )
+            cls.FILTER_CURRENT = cls.db.cursor.rowcount
+        
         cls.db.conn.commit()
-
-
-
-
-
+    
     @classmethod
     def skip_set(cls, csvfile):
         """
@@ -228,15 +250,17 @@ class SnapshotCollection:
                         WHERE timestamp = ? AND url_origin = ?
                         """
                 for row in csv_content:
-                    row_batch.append((
-                        row["url_archive"],
-                        row["redirect_url"],
-                        row["redirect_timestamp"],
-                        row["response"],
-                        row["file"],
-                        row["timestamp"],
-                        row["url_origin"]
-                    ))
+                    row_batch.append(
+                        (
+                            row["url_archive"],
+                            row["redirect_url"],
+                            row["redirect_timestamp"],
+                            row["response"],
+                            row["file"],
+                            row["timestamp"],
+                            row["url_origin"],
+                        )
+                    )
                     if len(row_batch) >= row_batchsize:
                         total_skipped += len(row_batch)
                         cls.db.cursor.executemany(query, row_batch)
@@ -246,28 +270,32 @@ class SnapshotCollection:
                     cls.db.cursor.executemany(query, row_batch)
                 cls.db.conn.commit()
                 cls.FILTER_SKIP = total_skipped
-
-
-
-
-
+    
     @classmethod
-    def count_totals(cls, total=False, handled=False, unhandled=False, success=False, fail=False):
+    def count_totals(
+        cls, total=False, handled=False, unhandled=False, success=False, fail=False
+    ):
         if total:
-            return cls.db.cursor.execute("SELECT COUNT(rowid) FROM snapshot_tbl").fetchone()[0]
+            return cls.db.cursor.execute(
+                "SELECT COUNT(rowid) FROM snapshot_tbl"
+            ).fetchone()[0]
         if handled:
-            return cls.db.cursor.execute("SELECT COUNT(rowid) FROM snapshot_tbl WHERE response IS NOT NULL").fetchone()[0]
+            return cls.db.cursor.execute(
+                "SELECT COUNT(rowid) FROM snapshot_tbl WHERE response IS NOT NULL"
+            ).fetchone()[0]
         if unhandled:
-            return cls.db.cursor.execute("SELECT COUNT(rowid) FROM snapshot_tbl WHERE response IS NULL").fetchone()[0]
+            return cls.db.cursor.execute(
+                "SELECT COUNT(rowid) FROM snapshot_tbl WHERE response IS NULL"
+            ).fetchone()[0]
         if success:
-            return cls.db.cursor.execute("SELECT COUNT(rowid) FROM snapshot_tbl WHERE file IS NOT NULL").fetchone()[0]
+            return cls.db.cursor.execute(
+                "SELECT COUNT(rowid) FROM snapshot_tbl WHERE file IS NOT NULL"
+            ).fetchone()[0]
         if fail:
-            return cls.db.cursor.execute("SELECT COUNT(rowid) FROM snapshot_tbl WHERE file IS NULL").fetchone()[0]
-
-
-
-
-
+            return cls.db.cursor.execute(
+                "SELECT COUNT(rowid) FROM snapshot_tbl WHERE file IS NULL"
+            ).fetchone()[0]
+    
     def modify_snapshot(connection, snapshot_id, column, value):
         """
         Modify a snapshot-row in the snapshot table.
@@ -278,10 +306,10 @@ class SnapshotCollection:
             SET {column} = ?
             WHERE rowid = ?
             """,
-            (value, snapshot_id)
+            (value, snapshot_id),
         )
         connection.conn.commit()
-
+    
     def get_snapshot(connection):
         """
         Get a snapshot-row from the snapshot table with response NULL. (not processed)
@@ -293,13 +321,17 @@ class SnapshotCollection:
         )
         row = connection.cursor.fetchone()
         return row
-
+    
     @classmethod
     def create_output(cls, url: str, timestamp: str, output: str):
         """
         Create a file path for the snapshot.
         """
         domain, subdir, filename = url_split(url.split("id_/")[1], index=True)
-        download_dir = os.path.join(output, domain, subdir) if cls.MODE_CURRENT else os.path.join(output, domain, timestamp, subdir)
+        download_dir = (
+            os.path.join(output, domain, subdir)
+            if cls.MODE_CURRENT
+            else os.path.join(output, domain, timestamp, subdir)
+        )
         download_file = os.path.abspath(os.path.join(download_dir, filename))
         return download_file
