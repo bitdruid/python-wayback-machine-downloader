3aa977e0be91eb725fc10eecc79a56c4d80e76b9
bitdruid <bitdruid@outlook.com>
2026-05-07 23:15:31 +0200

    test interactive mode; dedup args for this

--- pywaybackup/main.py
+++ pywaybackup/main.py
@@ -1,12 +1,20 @@
+import sys
+
 from pywaybackup import PyWayBackup
-from pywaybackup.Arguments import Arguments as args
+from pywaybackup.arg_parser import Arguments
+from pywaybackup.interactive import Interactive
 
 
 def cli():
-    cli_input = args()
+    interactive = len(sys.argv) <= 1
+    cli_input = Interactive() if interactive else Arguments()
     cli_args = cli_input.get_args()
     config = PyWayBackup(**cli_args)
-    config.run(daemon=False)
+    try:
+        config.run(daemon=False)
+    finally:
+        if interactive:
+            input("\nPress Enter to exit...")
 
 
 if __name__ == "__main__":
